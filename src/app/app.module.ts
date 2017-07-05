1c44c240e5ab643042f41f506d6330fcb1c3d8ea
Unknown <sebas.alem.f@gmail.com>
2017-07-05 17:32:20 -0500

    Component Layer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
